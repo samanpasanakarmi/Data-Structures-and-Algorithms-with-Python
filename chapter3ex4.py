25416c7d3d168aac08c53a2694f17928f650b2d9
Saman Pasa Nakarmi <samanpasa.nakarmi@metropolia.fi>
2026-01-27 13:11:39 +0200

    Update chapter3ex4.py

--- chapter3ex4.py
+++ chapter3ex4.py
@@ -1,17 +1,12 @@
-class ListNode:
-    def __init__(self, data=None, next=None, prev=None):
+class ListNode():
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
-        self.prev = prev
-
+    
     def __repr__(self):
         return f'<ListNode: {self.data}>'
 
-    def __str__(self):
-        return str(self.data)
-
-
-class DoublyLinkedList:
+class SinglyLinkedList():
     def __init__(self):
         self._head = self._tail = None
         self._size = 0
@@ -23,54 +18,10 @@ class DoublyLinkedList:
             values += f', {current_node.data}'
             current_node = current_node.next
         plural = '' if self._size == 1 else 's'
-        return f'<DoublyLinkedList ({self._size} element{plural}): [{values.lstrip(", ")}]>'
+        return f'<SinglyLinkedList ({self._size} element{plural}): [{values.lstrip(", ")}]>'
 
     def __len__(self):
         return self._size
-
-    def __iter__(self):
-        self._iter_index = self._head
-        return self
-
-    def __next__(self):
-        if self._iter_index:
-            value = self._iter_index.data
-            self._iter_index = self._iter_index.next
-            return value
-        else:
-            raise StopIteration
-            
-    def __getitem__(self, index):
-        """
-        Return value at index
-        """
-        # Check if index is inside bounds
-        if index < 0 or index >= self._size:
-            raise(ValueError('Index out of bounds'))
-
-        # Move to the given index
-        current_node = self._head
-        for _ in range(index):
-            current_node = current_node.next
-        
-        # Return the value
-        return current_node.data
-
-    def __setitem__(self, index, value):
-        """
-        set value at index k with val
-        """
-        # Check if index is inside bounds
-        if index < 0 or index >= self._size:
-            raise(ValueError('Index out of bounds'))
-
-        # Move to the given index
-        current_node = self._head
-        for _ in range(index):
-            current_node = current_node.next
-
-        # Set the value
-        current_node.data = value
 
     def append(self, value):
         """
@@ -81,21 +32,18 @@ class DoublyLinkedList:
 
         Returns: None
         """
-        # Create the node with the value. This is the last node, so next is None,
-        # but there can be already some node in the list, hence the prev value
-        new_node = ListNode(value, next=None, prev=self._tail)
+        # Create the node with the value
+        new_node = ListNode(value)
 
-        # If list is empty, update head and tail pointers
+        # If list is empty just point the header to the new node
         if self._head is None:
             self._head = self._tail = new_node
         else:
-            # In any other case, update tail node to point to the new element
-            # and update tail pointer. The new node already points to its
-            # previous element
+            # if list is not empty, update the last element and point it to the new node
             self._tail.next = new_node
             self._tail = new_node
-
-        # update size
+        
+        # Update list's size
         self._size += 1
 
     def pop(self):
@@ -107,88 +55,93 @@ class DoublyLinkedList:
         Returns:
             The content of the removed node. If list is empty, returns None
         """
-        # If list is empty, returns None
+        # If list is empty return None
         if not self._size:
             return None
         
         # Locate previous_node (the node just before last node)
-        node_to_remove = self._tail
-        previous_node = node_to_remove.prev
-
-        # If node to remove is first node, then update head pointer
-        if node_to_remove == self._tail:
-            self._head = None
+        if self._size == 1:
+            previous_node = None
         else:
-            # If not, update the pointer of the previous node
-            previous_node.next = None   # It is now the last node
+            previous_node = self._head
+            for _ in range(self._size-1):
+                    previous_node = previous_node.next
 
-        # Update tail pointer
+        # If head is also last node, then update head
+        if self._head == self._tail:
+            self._head = None
+
+        # Save the content of the last node and remove it
+        value = self._tail.data
+        del(self._tail)
+
+        # Update tail
         self._tail = previous_node
 
-        # Update size, remove node and return its content
+        # Finally update size and return the value of the removed node
         self._size -= 1
-        value = node_to_remove.data
-        del(node_to_remove)
         return value
 
-    def contains(self, value):
-        """
-        Returns True if value if found in the list and False if not
-        """
-        for node_value in self:
-            if value == node_value:
-                return True
-        return False
-
-    def clear(self):
-        """
-        Clear the list
-        """
-        # Remove all nodes
-        current_node = self._head
-        while current_node:
-            next = current_node.next
-            del(current_node)
-            current_node = next
-
-        # Update pointers and size
-        self._head = self._tail = None
-        self._size = 0
-
     def insert(self, index, value):
+        """
+        Insert a new node with value in the position given by the index
+
+        Parameters:
+        - 'index': The position where to insert the new node
+        - 'value': The value of the new node
+
+        Returns: None
+        """
+        # Check if index is inside bounds
         if index < 0 or index > self._size:
-            raise ValueError("Index out of range")
+            raise(ValueError('Index out of bounds'))
 
-        # insert at head
-        if index == 0:
-            new_node = ListNode(value, self._head)
-            if self._head:
-                self._head.prev = new_node
-            self._head = new_node
-            if self._size == 0:
-                self._tail = new_node
-            self._size += 1
-            return
-
-        # insert at tail (append)
-        if index == self._size:
-            new_node = ListNode(value, None)
-            new_node.prev = self._tail
-            self._tail.next = new_node
-            self._tail = new_node
-            self._size += 1
-            return
-
-        # insert in the middle
-        current = self._head
+        # Prepare some variables to make the necessary changes
+        # The new node will be inserted between previous_node and next_node
+        previous_node = None
+        next_node = self._head
+        # Move to the given index and update pointer variables
         for _ in range(index):
-            current = current.next
+            previous_node = next_node
+            next_node = next_node.next
 
-        previous = current.prev
-        new_node = ListNode(value, current)
+        # Create new node. It's next pointer points to next node or None
+        new_node = ListNode(value, next_node)
 
-        new_node.prev = previous
-        previous.next = new_node
-        current.prev = new_node
+        # If insert at front, update head
+        if previous_node is None:
+            self._head = new_node
+        else:
+            # If not, update previous node
+            previous_node.next = new_node
+        
+        # If insert at the end, update tail
+        if previous_node == self._tail:
+            self._tail = new_node
 
+        # Update list size
         self._size += 1
+
+    def remove(self, index):
+        if index < 0 or index >= self._size:
+            raise ValueError("Out of range")
+
+        previous_node = None
+        current_node = self._head
+
+        for _ in range(index):
+            previous_node = current_node
+            current_node = current_node.next
+        next_node = current_node.next
+
+        if current_node == self._head:
+            self._head = next_node
+        else:
+            previous_node.next = next_node
+        
+        if current_node == self._tail:
+            self._tail = previous_node
+        value= current_node.data
+        del (current_node)
+        self._size -=1
+        return value
